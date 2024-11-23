1551565ffc804f254722d1d6656159e2fd8e32f7
Alvien <Alvien.salvador@outlook.com>
2024-11-23 19:09:35 +1300

    Tweaked the search bar in the search screen so that when a user interacts with it, it transitions to the top of the screen

--- grocer-app/app/main/search.jsx
+++ grocer-app/app/main/search.jsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Animated} from 'react-native';
 import {useState} from 'react';
 import { SearchBar } from 'react-native-elements';
 import ContentContainer from '@/components/search/contentContainer'
@@ -9,10 +9,19 @@ export default function Search(){
     const [toSearch, setToSearch] = useState("");
 
     const style = StyleSheet.create({
-        mainView:{
+        mainViewNorm:{
             flex: 1,
             alignItems: 'center',
-            padding: 100,
+            rowGap: 10,
+            margin: 100,
+
+        },
+
+        mainViewSearch:{
+            flex: 1,
+            alignItems: 'center',
+            rowGap: 10,
+            margin: 10,
 
         },
 
@@ -32,7 +41,7 @@ export default function Search(){
         }
     });
     return(
-        <View style={style.mainView}>
+        <View style={!toSearch ? style.mainViewNorm : style.mainViewSearch}>
             <SearchBar 
             onChangeText={setToSearch}
             lightTheme={true}
@@ -41,7 +50,7 @@ export default function Search(){
             inputContainerStyle = {style.inputStyleContainer}
             placeholder='Milk...'
            />
-
+            {!toSearch ? 
            <ContentContainer>
                 <Text>
                     Good deals
@@ -52,6 +61,8 @@ export default function Search(){
                     ))
                 }
            </ContentContainer>
+            : <Text>
+                Searching for '"{toSearch}"</Text>}   
         </View>
     );
 }
